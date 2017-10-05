acce7cffad6458f9544491e939ffa32f58d9d2ac
Pablo Murcia Odriozola <pmurciao14@gmail.com>
2017-10-05 12:51:26 +0200

    Add play button

--- app/src/main/java/pablomurcia/tappydefender/MainActivity.java
+++ app/src/main/java/pablomurcia/tappydefender/MainActivity.java
@@ -1,13 +1,24 @@
 package pablomurcia.tappydefender;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
