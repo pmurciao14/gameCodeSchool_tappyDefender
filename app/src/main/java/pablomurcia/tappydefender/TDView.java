4b00a509afd5e15a5181960381ccac3d411e19b0
Pablo Murcia Odriozola <pmurciao14@gmail.com>
2017-10-05 13:13:19 +0200

    Add game thread and methods to control it

--- app/src/main/java/pablomurcia/tappydefender/TDView.java
+++ app/src/main/java/pablomurcia/tappydefender/TDView.java
@@ -1,7 +1,10 @@
 package pablomurcia.tappydefender;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.SurfaceView;
+
+import javax.security.auth.login.LoginException;
 
 /**
  * Created by ifbpmurcia.externos on 5/10/17.
@@ -9,12 +12,42 @@ import android.view.SurfaceView;
 
 public class TDView extends SurfaceView implements Runnable {
 
+    volatile boolean playing;
+    Thread gameThread = null;
+
     public TDView(Context context) {
         super(context);
     }
 
     @Override
     public void run() {
+        while (playing){
+            update();
+            //draw();
+            control();
+        }
+    }
 
+    private void control() {
+
+    }
+
+    private void update() {
+
+    }
+
+    public void resume() {
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void pause() {
+        playing = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e){
+            Log.e("----->", e.getMessage());
+        }
     }
 }
