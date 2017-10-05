ea3a42c70e4e52737aa1e4ba9c52b98cd9245198
Pablo Murcia Odriozola <pmurciao14@gmail.com>
2017-10-05 13:03:14 +0200

    Add View Class

---
+++ app/src/main/java/pablomurcia/tappydefender/TDView.java
@@ -0,0 +1,20 @@
+package pablomurcia.tappydefender;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+/**
+ * Created by ifbpmurcia.externos on 5/10/17.
+ */
+
+public class TDView extends SurfaceView implements Runnable {
+
+    public TDView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
